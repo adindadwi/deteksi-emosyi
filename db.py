17e0b64e9f651c4e9a240a5449bd86ad6cccc0e1
Adinda Dwi <larasatidwidinda@gmail.com>
2020-07-12 21:33:52 +0700

    change app db and main py

--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 from app import app
+from flaskext.mysql import MySQL
+
 # import mysql.connector
 
 # mydb = mysql.connector.connect(host="us-cdbr-east-02.cleardb.com", user="b494071167088b", passwd="f589afc7", database="heroku_35abaa3e25ea5b0")
-
-from flaskext.mysql import MySQL
 
 
 mydb = MySQL()
